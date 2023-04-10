8236c327088c840d3af59ec4f47ceacef423a0f9
olakunle-remi <abbeypentious@gmail.com>
2023-04-09 23:21:20 -0700

    1-isdigit.c and 1-memcpy.c update

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -4,7 +4,12 @@
 #include <ctype.h>
 #include <string.h>
 
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
 int _isdigit(int c)
 {
-    return isdigit(c);
+	return (c >= '0' && c <= '9');
 }
