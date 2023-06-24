cce37d5c5b8088432fec728fce6ae4cf9e3413fc
olakunle-remi <abbeypentious@gmail.com>
2023-06-23 21:11:31 -0700

    100-operation update

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - program to add a and b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * sub - program to subtract a and b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mul - program to multiply a and b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - program to divide a and b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * mod - program to modulus a and b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+    return (a % b);
+}
